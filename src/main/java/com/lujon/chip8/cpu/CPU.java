fc9b71a5ee735270171fccf31cfc08d6525496d1
Ludvig Jonsson <github@ludvig.email>
2021-06-02 12:46:38 +0200

    Refactor CPU

--- src/main/java/com/lujon/chip8/cpu/CPU.java
+++ src/main/java/com/lujon/chip8/cpu/CPU.java
@@ -10,7 +10,7 @@ public class CPU {
   private final Memory memory;
   private final Screen screen;
   private final int[] registers = new int[16];
-  private int pc = INITIAL_PC;
+  private int programCounter = INITIAL_PC;
   private int indexRegister;
 
   public CPU(Memory memory, Screen screen) {
@@ -24,20 +24,20 @@ public class CPU {
   }
 
   private Instruction fetchInstruction() {
-    int instruction = (memory.getByte(pc) << 8) | (memory.getByte(pc + 1) & 0xFF);
-    pc += 2;
+    int instruction = (memory.getByte(programCounter) << 8) | (memory.getByte(programCounter + 1) & 0xFF);
+    programCounter += 2;
     return new Instruction(instruction);
   }
 
   private void executeInstruction(Instruction instruction) {
     switch (instruction.getOpCode()) {
       case 0x0:
-        if (instruction.getNN() == 0x00E0) {
-          screen.clear();
+        if (instruction.getNN() == 0xE0) {
+          clearScreen();
         }
         break;
       case 0x1:
-        pc = instruction.getNNN();
+        jumpToAddress(instruction.getNNN());
         break;
       case 0x2:
         break;
@@ -48,66 +48,24 @@ public class CPU {
       case 0x5:
         break;
       case 0x6:
-        registers[instruction.getX()] = instruction.getNN();
+        setRegister(instruction.getX(), instruction.getNN());
         break;
       case 0x7:
-        registers[instruction.getX()] += instruction.getNN();
+        addToRegister(instruction.getX(), instruction.getNN());
         break;
       case 0x8:
         break;
       case 0x9:
         break;
       case 0xA:
-        indexRegister = instruction.getNNN();
+        setIndexRegister(instruction.getNNN());
         break;
       case 0xB:
         break;
       case 0xC:
         break;
       case 0xD:
-        int xCoord = (registers[instruction.getX()]) % 64;
-        int yCoord = (registers[instruction.getY()]) % 32;
-
-        registers[0xF] = 0;
-
-        for (int pixelRow = 0; pixelRow < instruction.getN(); pixelRow++) {
-          byte sprite = memory.getByte(indexRegister + pixelRow);
-
-          boolean[] bits = new boolean[] {
-              (sprite & 0b10000000) != 0,
-              (sprite & 0b01000000) != 0,
-              (sprite & 0b00100000) != 0,
-              (sprite & 0b00010000) != 0,
-              (sprite & 0b00001000) != 0,
-              (sprite & 0b00000100) != 0,
-              (sprite & 0b00000010) != 0,
-              (sprite & 0b00000001) != 0,
-          };
-
-          int screenYCoord = yCoord + pixelRow;
-
-          for (int bitIndex = 0; bitIndex < 8; bitIndex++) {
-            int screenXCoord = xCoord + bitIndex;
-
-            boolean previousPixel = screen.getPixel(screenXCoord, screenYCoord);
-            boolean newPixel = bits[bitIndex];
-
-            screen.setPixel(screenXCoord, screenYCoord, !previousPixel && newPixel);
-
-            if(previousPixel && newPixel){
-              registers[0xF] = (byte)0x01;
-            }
-
-            if (screenXCoord == screen.getWidth() - 1) {
-              break;
-            }
-          }
-
-          if (screenYCoord == screen.getHeight() - 1) {
-            break;
-          }
-        }
-
+        drawSprite(instruction.getX(), instruction.getY(), instruction.getN());
         break;
       case 0xE:
         break;
@@ -115,6 +73,77 @@ public class CPU {
         break;
       default:
         break;
+    }
+  }
+
+  // 00E0 - CLS
+  private void clearScreen() {
+    screen.clear();
+  }
+
+  // 1nnn - JP addr
+  private void jumpToAddress(int address) {
+    programCounter = address;
+  }
+
+  // 6xkk - LD Vx, byte
+  private void setRegister(int register, int value) {
+    registers[register] = value;
+  }
+
+  // 7xkk - ADD Vx, byte
+  private void addToRegister(int register, int value) {
+    registers[register] += value;
+  }
+
+  // Annn - LD I, addr
+  private void setIndexRegister(int address) {
+    indexRegister = address;
+  }
+
+  // Dxyn - DRW Vx, Vy, nibble
+  private void drawSprite(int xRegister, int yRegister, int numRows) {
+    int x = registers[xRegister] % 64;
+    int y = registers[yRegister] % 32;
+
+    registers[0xF] = 0;
+
+    for (int row = 0; row < numRows; row++) {
+      byte sprite = memory.getByte(indexRegister + row);
+
+      boolean[] bits = new boolean[] {
+          (sprite & 0b10000000) != 0,
+          (sprite & 0b01000000) != 0,
+          (sprite & 0b00100000) != 0,
+          (sprite & 0b00010000) != 0,
+          (sprite & 0b00001000) != 0,
+          (sprite & 0b00000100) != 0,
+          (sprite & 0b00000010) != 0,
+          (sprite & 0b00000001) != 0,
+      };
+
+      int screenYCoord = y + row;
+
+      for (int bitIndex = 0; bitIndex < 8; bitIndex++) {
+        int screenXCoord = x + bitIndex;
+
+        boolean previousPixel = screen.getPixel(screenXCoord, screenYCoord);
+        boolean newPixel = bits[bitIndex];
+
+        screen.setPixel(screenXCoord, screenYCoord, !previousPixel && newPixel);
+
+        if(previousPixel && newPixel){
+          registers[0xF] = (byte)0x01;
+        }
+
+        if (screenXCoord == screen.getWidth() - 1) {
+          break;
+        }
+      }
+
+      if (screenYCoord == screen.getHeight() - 1) {
+        break;
+      }
     }
   }
 }
